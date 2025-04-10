2c9a508190af09f47d375cddb4730d39ed676663
ysrastogi <ysrastogi19@gmail.com>
2025-04-10 23:17:50 +0530

    Add main application structure with routing and global state management

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import Home from "@/pages/Home";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
